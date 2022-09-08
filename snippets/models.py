146a78bfa427bf6e3389c551cf7c8b1cd1d006c2
AssetK <kopbayevasset@gmail.com>
2022-09-08 14:28:30 +0600

    add information to models

--- snippets/models.py
+++ snippets/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from pygments import highlight
 from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
 
 # Create your models here.
 
@@ -9,6 +12,9 @@ LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
 class Snippet(models.Model):
+    owner = models.ForeignKey('auth.User', related_name='snippets', 
+                                on_delete=models.CASCADE, default='')
+    highlighted = models.TextField(default='')
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
@@ -18,6 +24,14 @@ class Snippet(models.Model):
 
     class Meta:
         ordering = ['created']
+
+    def save(self, *args, **kwargs):
+        lexer = get_lexer_by_name(self.language)
+        linenos = 'table' if self.linenos else False
+        options = {'title': self.title} if self.title else {}
+        formatter = HtmlFormatter(style=self.style, linenos=linenos, full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super().save(*args, **kwargs)
 
 
 
