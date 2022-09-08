34e0efc745adb4a2751b33a25e817a0fbfc6db09
AssetK <kopbayevasset@gmail.com>
2022-09-08 15:48:20 +0600

    level permissions

--- snippets/views.py
+++ snippets/views.py
@@ -3,6 +3,8 @@ from rest_framework import permissions
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from snippets.serializers import UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
+
 
 from django.contrib.auth.models import User
 
@@ -24,7 +26,7 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
