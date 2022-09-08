7cf31b5df1c2981539cc007d3e84e4683b8cf5ae
AssetK <kopbayevasset@gmail.com>
2022-09-08 15:36:44 +0600

    authentification and permission realization

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+
     class Meta:
+        owner = Snippet
         model = Snippet
         fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
     
@@ -20,4 +23,12 @@ class SnippetSerializer(serializers.ModelSerializer):
         instance.save()
         return instance
 
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, 
+                                                    queryset=Snippet.objects.all())
+
+    owner = serializers.ReadOnlyField(source='owner.username')
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets']
 
