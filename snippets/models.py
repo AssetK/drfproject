db3caad75aa323e2804dcf5f2ac748e9c1e3d8b2
AssetK <kopbayevasset@gmail.com>
2022-09-09 10:59:11 +0600

    fix login page

--- snippets/models.py
+++ snippets/models.py
@@ -12,8 +12,7 @@ LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
 class Snippet(models.Model):
-    owner = models.ForeignKey('auth.User', related_name='snippets', 
-                                on_delete=models.CASCADE, default='')
+    owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE, default='')
     highlighted = models.TextField(default='')
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
